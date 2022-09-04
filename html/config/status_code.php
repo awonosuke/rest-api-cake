fd130a8275cd20a85d73cb0d4ee8b20c6b05cd07
awonosuke <blueno091117@gmail.com>
2022-09-05 03:28:19 +0900

    [add] define status code

---
+++ html/config/status_code.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+// https://developer.mozilla.org/ja/docs/Web/HTTP/Status
+
+// OK
+const StatusOK = 200;
+
+// Client error
+const StatusBadRequest = 400;
+const StatusUnauthorized = 401;
+const StatusForbidden = 403;
+const StatusNotFound = 404;
+const StatusMethodNotAllowed = 405;
+
+// Server error
+const StatusInternalServerError = 500;
+const StatusNotImplemented = 501;
+const StatusBadGateway = 502;
+const StatusServerUnavailable = 503;
+
+return array();
