f9f19a167c68fee74f5fe73e61be482c0dc3754f
awonosuke <blueno091117@gmail.com>
2022-09-04 16:45:58 +0900

    [add] prepare own difine exception

---
+++ html/src/Error/Exception/InvalidBodyException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Error\Exception;
+
+use Cake\Http\Exception\HttpException;
+
+class InvalidBodyException extends HttpException
+{
+    // define exception
+}
---
+++ html/src/Error/Exception/InvalidCodeException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Error\Exception;
+
+use Cake\Http\Exception\HttpException;
+
+class InvalidCodeException extends HttpException
+{
+    // define exception
+}
