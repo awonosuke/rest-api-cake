1413ccb11677920a11d10e761bc584c46e4bb5b8
awonosuke <blueno091117@gmail.com>
2022-09-10 15:45:40 +0900

    [change] prepare forced resign method

--- html/src/Controller/AdminController.php
+++ html/src/Controller/AdminController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Library\Response;
+use Cake\Datasource\Exception\RecordNotFoundException;
 use Cake\Event\EventInterface;
+use Cake\Http\Exception\BadRequestException;
 use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\MethodNotAllowedException;
 
 /**
  * Admin Controller
@@ -45,6 +48,36 @@ class AdminController extends AppController
         }
 
         $response = new Response(StatusOK, $request_url, (object) ['message' => 'Get all user', 'user' => $all_user]);
+        return $this->renderJson($response->formatResponse());
+    }
+
+    /**
+     * Forced resign target user
+     *
+     * @param int $user_id
+     * @return \Cake\Http\Response
+     */
+    public function forcedResignApi(int $user_id): \Cake\Http\Response
+    {
+        if (!$this->request->is('POST')) throw new MethodNotAllowedException('POST');
+        if (!$this->isAdmin()) throw new ForbiddenException();
+
+        // 自分を強制退会させることを禁止
+        if ($user_id === $this->loginUser['id']) throw new BadRequestException();
+
+        $request_url = $this->request->getRequestTarget();
+
+        $target_user = $this->Users->find()->where(['id' => $user_id])->first();
+        // rootユーザーの削除禁止
+        if ($target_user['email'] === 'root@example.com') throw new BadRequestException();
+        if (empty($target_user)) throw new RecordNotFoundException();
+
+        if ($this->Users->delete($target_user)) {
+            $response = new Response(StatusOK, $request_url, (object) ['message' => 'Forced resign target user']);
+            return $this->renderJson($response->formatResponse());
+        }
+
+        $response = new Response(StatusOK, $request_url, (object) ['message' => 'Failed forced resign target user']);
         return $this->renderJson($response->formatResponse());
     }
 }
