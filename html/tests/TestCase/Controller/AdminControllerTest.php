5fcdc88e3e8231f7e9894f520329e68ffcad0208
awonosuke <blueno091117@gmail.com>
2022-09-13 23:00:20 +0900

    [add] add sql file for setup test table

---
+++ html/tests/TestCase/Controller/AdminControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\SnippetsController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\SnippetsController Test Case
+ *
+ * @uses \App\Controller\SnippetsController
+ */
+class SnippetsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array<string>
+     */
+    protected $fixtures = [
+        'app.Snippets',
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     * @uses \App\Controller\SnippetsController::index()
+     */
+    public function testCreateSnippetApi(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     * @uses \App\Controller\SnippetsController::view()
+     */
+    public function testGetSnippetApi(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     * @uses \App\Controller\SnippetsController::add()
+     */
+    public function testAllSnippetApi(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     * @uses \App\Controller\SnippetsController::delete()
+     */
+    public function testAllExpireSnippetApi(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
