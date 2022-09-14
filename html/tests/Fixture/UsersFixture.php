3e5f8f6ce7d0cd5840da93592e2001739dac6707
awonosuke <blueno091117@gmail.com>
2022-09-15 00:28:29 +0900

    [change] update test data

--- html/tests/Fixture/UsersFixture.php
+++ html/tests/Fixture/UsersFixture.php
@@ -20,12 +20,22 @@ class UsersFixture extends TestFixture
         $this->records = [
             [
                 'id' => 1,
+                'email' => 'root@example.com',
+                'password' => '$2y$10$eHPTMoPNLtgYidm7j8mOZOs3uK0sYpG9d8kYa.rjTLsVI.TMqNSji',
+                'user_name' => 'root',
+                'role' => 'admin',
+                'created' => '2022-09-13 23:31:13',
+                'updated' => '2022-09-13 23:31:13'
+            ],
+            [
+                'id' => 2,
                 'email' => 'test@example.com',
                 'password' => '$2y$10$Afyt31Owvbm4/oeL9C2LmOYOCLJntrEIgkfJOyNmrLsSfuPlcVpqq',
                 'user_name' => 'test',
+                'role' => 'user',
                 'created' => '2022-09-13 23:31:13',
-                'updated' => '2022-09-13 23:31:13',
-            ],
+                'updated' => '2022-09-13 23:31:13'
+            ]
         ];
         parent::init();
     }
