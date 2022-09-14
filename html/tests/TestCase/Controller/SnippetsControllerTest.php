42756fed864bd325eb2f75f36d2aee88f8cf07bf
awonosuke <blueno091117@gmail.com>
2022-09-14 23:55:04 +0900

    [change] add snippet controller test

--- html/tests/TestCase/Controller/SnippetsControllerTest.php
+++ html/tests/TestCase/Controller/SnippetsControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Test\TestCase\Controller;
 
 use App\Controller\SnippetsController;
+use Cake\I18n\FrozenTime;
 use Cake\TestSuite\IntegrationTestTrait;
 use Cake\TestSuite\TestCase;
 
@@ -34,7 +35,34 @@ class SnippetsControllerTest extends TestCase
      */
     public function testCreateSnippetApi(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $url = '/snippet/create';
+
+        // Unauthorized
+        $this->post($url);
+        $this->assertResponseCode(StatusUnauthorized);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Unauthorized');
+
+        // OK
+        $this->post($url, [
+            'content' => 'An old silent pond',
+            'expire' => (new FrozenTime('+3 days'))->i18nFormat('yyyy-MM-dd HH:mm:ss')
+        ]);
+        $this->assertResponseOk();
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Create a new snippet');
+
+        // Method Not Allowed
+        $this->get($url);
+        $this->assertResponseCode(StatusMethodNotAllowed);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Method Not Allowed');
+
+        // Validation Unique Constraint (email)
+        $this->post($url);
+        $this->assertResponseCode(StatusUnprocessableEntity);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Validation error occur');
     }
 
     /**
@@ -45,7 +73,31 @@ class SnippetsControllerTest extends TestCase
      */
     public function testGetSnippetApi(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $url = '/snippet/1';
+
+        // Unauthorized
+        $this->get($url);
+        $this->assertResponseCode(StatusUnauthorized);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Unauthorized');
+
+        // OK
+        $this->get($url);
+        $this->assertResponseOk();
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Get a snippet');
+
+        // Record not found
+        $this->get('/snippet/0');
+        $this->assertResponseOk();
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Record not found');
+
+        // Method Not Allowed
+        $this->post($url);
+        $this->assertResponseCode(StatusMethodNotAllowed);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Method Not Allowed');
     }
 
     /**
@@ -56,17 +108,24 @@ class SnippetsControllerTest extends TestCase
      */
     public function testAllSnippetApi(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
+        $url = '/snippet/all';
 
-    /**
-     * Test delete method
-     *
-     * @return void
-     * @uses \App\Controller\SnippetsController::delete()
-     */
-    public function testAllExpireSnippetApi(): void
-    {
-        $this->markTestIncomplete('Not implemented yet.');
+        // Unauthorized
+        $this->get($url);
+        $this->assertResponseCode(StatusUnauthorized);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Unauthorized');
+
+        // OK
+        $this->get($url);
+        $this->assertResponseOk();
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Get all snippet');
+
+        // Method Not Allowed
+        $this->post($url);
+        $this->assertResponseCode(StatusMethodNotAllowed);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Method Not Allowed');
     }
 }
