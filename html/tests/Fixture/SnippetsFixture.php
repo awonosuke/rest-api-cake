c476998f264801ff0a024ace7e0def9e312dc5b8
awonosuke <blueno091117@gmail.com>
2022-09-15 23:57:59 +0900

    [change] update snippet fixture

--- html/tests/Fixture/SnippetsFixture.php
+++ html/tests/Fixture/SnippetsFixture.php
@@ -24,8 +24,22 @@ class SnippetsFixture extends TestFixture
                 'user_id' => 1,
                 'content' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
                 'expire' => (new FrozenTime('+3 days'))->i18nFormat('yyyy-MM-dd HH:mm:ss'),
-                'created' => FrozenTime::now()->i18nFormat('yyyy-MM-dd HH:mm:ss'),
+                'created' => FrozenTime::now()->i18nFormat('yyyy-MM-dd HH:mm:ss')
             ],
+            [
+                'id' => 2,
+                'user_id' => 1,
+                'content' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'expire' => (new FrozenTime('-3 days'))->i18nFormat('yyyy-MM-dd HH:mm:ss'),
+                'created' => FrozenTime::now()->i18nFormat('yyyy-MM-dd HH:mm:ss')
+            ],
+            [
+                'id' => 3,
+                'user_id' => 2,
+                'content' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'expire' => (new FrozenTime('+3 days'))->i18nFormat('yyyy-MM-dd HH:mm:ss'),
+                'created' => FrozenTime::now()->i18nFormat('yyyy-MM-dd HH:mm:ss')
+            ]
         ];
         parent::init();
     }
