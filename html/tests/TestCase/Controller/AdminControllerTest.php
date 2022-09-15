8b744a5b1dd9bb27102733fb607603bc45f8150f
awonosuke <blueno091117@gmail.com>
2022-09-16 02:43:57 +0900

    [change] update test case

--- html/tests/TestCase/Controller/AdminControllerTest.php
+++ html/tests/TestCase/Controller/AdminControllerTest.php
@@ -27,6 +27,42 @@ class AdminControllerTest extends TestCase
     ];
 
     /**
+     * Get JWT method
+     *
+     * @param string $role 'root'|'admin'|'test'
+     * @return string
+     */
+    private function getJsonWebToken(string $role): string
+    {
+        $this->post('/user/login', [
+            'email' => $role . '@example.com',
+            'password' => $role,
+        ]);
+        $response_body = json_decode((string) $this->_response->getBody(), true);
+        return $response_body['body']['token'];
+    }
+
+    private function setRequestHeaders(string $jwt)
+    {
+        $this->configRequest([
+            'headers' => ['Authorization' => 'Bearer ' . $jwt]
+        ]);
+    }
+
+    /**
+     * Expire JWT method byb sending logout request
+     *
+     * @param string $jwt
+     * @return void
+     */
+    private function expireJsonWebToken(string $jwt)
+    {
+        $this->setRequestHeaders($jwt);
+        $this->post('/user/logout');
+    }
+
+
+    /**
      * Test index method
      *
      * @return void
@@ -34,25 +70,47 @@ class AdminControllerTest extends TestCase
      */
     public function testMakeAdminUserApi(): void
     {
-        $url = '/admin/make-admin/';
+        $url = '/admin/user/make-admin/';
+
+        // Unauthorized
+        $this->post($url . '3');
+        $this->assertResponseCode(StatusUnauthorized);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Unauthorized');
+
+        $jwt = $this->getJsonWebToken('admin');
 
         // OK
-        $this->post($url . '2');
+        $this->setRequestHeaders($jwt);
+        $this->post($url . '4');
         $this->assertResponseOk();
         $this->assertContentType('application/json');
         $this->assertResponseContains('Make new admin user');
 
         // Bad Request
-        $this->post($url . '2');
+        $this->setRequestHeaders($jwt);
+        $this->post($url . '1');
         $this->assertResponseCode(StatusBadRequest);
         $this->assertContentType('application/json');
         $this->assertResponseContains('Bad Request');
 
         // Method Not Allowed
-        $this->get($url . '2');
+        $this->setRequestHeaders($jwt);
+        $this->get($url . '4');
         $this->assertResponseCode(StatusMethodNotAllowed);
         $this->assertContentType('application/json');
         $this->assertResponseContains('Method Not Allowed');
+
+        $this->expireJsonWebToken($jwt);
+
+        // Forbidden
+        $jwt = $this->getJsonWebToken('test');
+        $this->setRequestHeaders($jwt);
+        $this->post($url . '4');
+        $this->assertResponseCode(StatusForbidden);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Forbidden');
+        $this->expireJsonWebToken($jwt);
     }
 
     /**
@@ -65,17 +123,29 @@ class AdminControllerTest extends TestCase
     {
         $url = '/admin/user/all';
 
+        // Unauthorized
+        $this->get($url);
+        $this->assertResponseCode(StatusUnauthorized);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Unauthorized');
+
+        $jwt = $this->getJsonWebToken('admin');
+
         // OK
+        $this->setRequestHeaders($jwt);
         $this->get($url);
         $this->assertResponseOk();
         $this->assertContentType('application/json');
         $this->assertResponseContains('Get all user');
 
         // Method Not Allowed
+        $this->setRequestHeaders($jwt);
         $this->post($url);
         $this->assertResponseCode(StatusMethodNotAllowed);
         $this->assertContentType('application/json');
         $this->assertResponseContains('Method Not Allowed');
+
+        $this->expireJsonWebToken($jwt);
     }
 
     /**
@@ -88,23 +158,43 @@ class AdminControllerTest extends TestCase
     {
         $url = '/admin/user/forced-resign/';
 
+        // Unauthorized
+        $this->post($url . '4');
+        $this->assertResponseCode(StatusUnauthorized);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Unauthorized');
+
+        $jwt = $this->getJsonWebToken('admin');
+
         // OK
-        $this->post($url . '2');
+        $this->setRequestHeaders($jwt);
+        $this->post($url . '4');
         $this->assertResponseOk();
         $this->assertContentType('application/json');
         $this->assertResponseContains('Forced resign target user');
 
         // Record Not Found
+        $this->setRequestHeaders($jwt);
         $this->post($url . '0');
         $this->assertResponseCode(StatusOK);
         $this->assertContentType('application/json');
         $this->assertResponseContains('Record Not Found');
 
+        // Bad Request
+        $this->setRequestHeaders($jwt);
+        $this->post($url . '1');
+        $this->assertResponseCode(StatusBadRequest);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Bad Request');
+
         // Method Not Allowed
-        $this->get($url . '2');
+        $this->setRequestHeaders($jwt);
+        $this->get($url . '3');
         $this->assertResponseCode(StatusMethodNotAllowed);
         $this->assertContentType('application/json');
         $this->assertResponseContains('Method Not Allowed');
+
+        $this->expireJsonWebToken($jwt);
     }
 
     /**
@@ -117,22 +207,35 @@ class AdminControllerTest extends TestCase
     {
         $url = '/admin/snippet/forced-delete/';
 
+        // Unauthorized
+        $this->post($url . '4');
+        $this->assertResponseCode(StatusUnauthorized);
+        $this->assertContentType('application/json');
+        $this->assertResponseContains('Unauthorized');
+
+        $jwt = $this->getJsonWebToken('admin');
+
         // OK
+        $this->setRequestHeaders($jwt);
         $this->post($url . '1');
         $this->assertResponseOk();
         $this->assertContentType('application/json');
-        $this->assertResponseContains('Make new admin user');
+        $this->assertResponseContains('Forced delete target snippet');
 
         // Record Not Found
+        $this->setRequestHeaders($jwt);
         $this->post($url . '0');
         $this->assertResponseCode(StatusOK);
         $this->assertContentType('application/json');
         $this->assertResponseContains('Record Not Found');
 
         // Method Not Allowed
+        $this->setRequestHeaders($jwt);
         $this->get($url . '1');
         $this->assertResponseCode(StatusMethodNotAllowed);
         $this->assertContentType('application/json');
         $this->assertResponseContains('Method Not Allowed');
+
+        $this->expireJsonWebToken($jwt);
     }
 }
